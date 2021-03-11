0843c34e3434e50a91d2d2d20a31e69cd9766d82
KornelDob <kordob@outlook.com>
2021-03-11 00:19:20 +0000

    MOVEMENT

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float Speed = 20f;
+    public int randomIndex;
+    public int Levels;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomIndex = Random.Range(0, 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        if (Input.GetKey("d"))
+        {
+            pos.x += Speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= Speed * Time.deltaTime;
+        }
+        transform.position = pos;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "door")
+        {
+            if (randomIndex == 0)
+            {
+
+            }
+            if (randomIndex == 1)
+            {
+
+            }
+            if (randomIndex == 2)
+            {
+
+            }
+            if (randomIndex == 3)
+            {
+
+            }
+        }
+
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2d1 controller;
+
+    float horizontalMove = 0f;
+    public float runSpeed = 100f;
+
+    bool jump = false;
+    bool crouch = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
